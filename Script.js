cd5be550a86efcbfb9c981a0017ed093425aea0d
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-10-05 18:41:13 +0600

    navigation related some issue fix

--- Script.js
+++ Script.js
@@ -1,5 +1,27 @@
-// nav menu related 
-const handleToggle = () =>{
-   const menu = document.getElementById('menu');
-   menu.classList.toggle("show")
-}
+// toggling nav related
+const handleToggle = () => {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("show");
+};
+
+//smooth scrolling nav bg color change
+const nav = document.querySelector("nav");
+
+const handleScroll = () => {
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
+
+//action link action
+const menuLinks = document.querySelectorAll(".menu ul li a");
+menuLinks.forEach((menu) => {
+  menu.addEventListener("click", function (e) {
+    menuLinks.forEach((items) => items.classList.remove("text_red"));
+    this.classList.add("text_red");
+  });
+});
