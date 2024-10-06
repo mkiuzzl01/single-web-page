10135128095fb730b028ebcb251f39222e3514ed
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-10-06 23:47:03 +0600

    dynamically get json data and push html file

--- Script.js
+++ Script.js
@@ -1,3 +1,47 @@
+<<<<<<< HEAD
+=======
+//fetch data form asset file
+// =============================================
+const handleGetData = async () => {
+  const cardContainer = document.getElementById("card_container");
+  fetch("/assets/Products.json")
+    .then((res) => res.json())
+    .then((json) => {
+      let data = json;
+
+      //find each data a object formate
+      data.forEach((item) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        // create a card for HTML Document
+        card.innerHTML = `
+            <div class="card_image">
+              <img
+                src="${item?.ProductImage}"
+                alt="${item?.ProductName}"
+              />
+            </div>
+            <div class="card_content">
+              <h4>${item?.ProductName}</h4>
+              <p>${item?.Description}</p>
+              <div class="card_label">
+              <p class="price">$${item?.Price}</p>
+              <p class="rating">Rating:${item?.Ratings}</p>
+              </div>
+              <button class="buy_btn">Buy Now</button>
+            </div>
+      `;
+      // and push the card the html file
+        cardContainer.appendChild(card);
+        // console.log(item);
+      });
+    });
+};
+
+handleGetData();
+
+>>>>>>> 3bffe0e (add to json data and dynamically render the data with card in html document)
 // toggling nav related
 const handleToggle = () => {
   const menu = document.getElementById("menu");
